75d6669d48bd4f7bc0305d73ed257f1630c10437
m <xiaoham3@uci.edu>
2018-03-06 20:22:08 -0800

    56

---
+++ Array/56-merge-intervals.cc
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<Interval> merge(vector<Interval>& intervals) {
+        vector<Interval> res;
+        if (intervals.size() == 0) return res;
+        // sort intervals by start
+        sort(intervals.begin(), intervals.end(), [](Interval a, Interval b) {return a.start < b.start;});
+        // put [0] into res
+        res.push_back(intervals[0]);
+        // for i=1 -> n-1
+        for (int i = 1; i < intervals.size(); i++) {
+            // if i overlaps with [i-1]
+            if (intervals[i].start <= res.back().end) 
+            // merge (change the end of [i-1])
+                res.back().end = max(res.back().end, intervals[i].end);
+            // else (not overlap)
+            else
+            // push back [i-1] to res
+                res.push_back(intervals[i]);
+        }
+        return res;
+    }
+};
